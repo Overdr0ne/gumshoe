900d18640cd8c43ce5a8c66ad2c967c83cbd8d6c
Overdr0ne <scmorris.dev@gmail.com>
2024-09-02 17:15:15 -0700

    factor tree functions into separate module

---
+++ etree.el
@@ -0,0 +1,131 @@
+;;; etree.el --- A library for working with trees    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Sam
+
+;; Author: Sam <scmorris.dev@gmail.com>
+;; Keywords: internal, convenience, convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a library for working with trees in a mutable, object-oriented
+;; style.
+
+;;; Code:
+
+(defclass etree--node ()
+  (
+   (entry :initform nil
+          :initarg :entry)
+   (children :initform nil
+             :initarg :children)
+   (parent :initform nil
+           :initarg :parent))
+  "A node in a tree.")
+
+(defclass etree--tree ()
+  (
+   (root :initform nil
+         :initarg :root
+         :documentation "Root of the tree.")
+   (current :initform nil
+            :initarg :current
+            :documentation "Current node in the tree."))
+  "A tree to keep track of the target’s movements as they move forward and backward in time.")
+
+(cl-defmethod etree--insert ((self etree--tree) (new etree--node))
+  (if (not self)
+      (etree--tree self :root new :current new)
+    (if (not (oref self root))
+        (progn (oset self root new) (oset self current new))
+      (let* ((current (oref self current)))
+        (oset current children (cons new (oref current children)))
+        (oset new parent current)
+        (oset self current new)
+        )))
+  self
+  )
+
+(cl-defmethod etree--preorder ((self etree--tree))
+  (let* (
+         (stk (list (oref self root)))
+         (preorder nil)
+         (i 0)
+         iter
+         )
+    (while stk
+      (setq iter (pop stk))
+      (push iter preorder)
+      (dolist (child (oref iter children)) (setf stk (cons child stk)))
+      (cl-incf i))
+    preorder)  )
+
+(cl-defmethod etree--dfs ((self etree--tree) (key etree--node))
+  (let* (
+         (stk (list (oref self root)))
+         (i 0)
+         iter
+         (continuep t)
+         foundp)
+    (while (and stk continuep)
+      (setq iter (pop stk))
+      (dolist (child (oref iter children)) (setf stk (cons child stk)))
+      (when (eq iter key)
+        (setq continuep nil)
+        (setf foundp t))
+      (cl-incf i))
+    foundp))
+
+(cl-defmethod etree--delete ((self etree--node))
+  (when self
+    (when (oref self parent)
+      (oset (oref self parent) children (oref self children)))
+    (dolist (child (oref self children))
+      (oset child parent (oref self parent)))))
+
+(cl-defmethod etree--remove ((self etree--tree) (key etree--node))
+  (when (eq key (oref self current))
+    (oset self current (oref (oref self current) parent)))
+  (when (eq key (oref self root))
+    (oset self root (car (oref (oref self root) children))))
+  (etree--delete key))
+
+(cl-defmethod etree--mapc ((self etree--tree) f)
+  (let* ((stk (list (oref self root)))
+         (preorder nil)
+         (i 0)
+         iter)
+    (while stk
+      (setq iter (pop stk))
+      (funcall f iter)
+      (push iter preorder)
+      (dolist (child (oref iter children)) (setf stk (cons child stk)))
+      (cl-incf i))
+    preorder))
+
+(cl-defmethod etree--collect ((self etree--tree) predicate)
+  (let* ((stk (list (oref self root)))
+         (preorder nil)
+         (i 0)
+         iter)
+    (while (and stk (car stk))
+      (setq iter (pop stk))
+      (when (funcall predicate iter)
+        (push iter preorder))
+      (dolist (child (oref iter children)) (setf stk (cons child stk)))
+      (cl-incf i))
+    preorder))
+(provide 'etree)
+;;; etree.el ends here
--- gumshoe-tree.el
+++ gumshoe-tree.el
@@ -1,83 +1,7 @@
 (require 'dash)
+(require 'etree)
 
-(defclass gumshoe--node ()
-  (
-   (entry :initform (gumshoe--entry)
-          :initarg :entry)
-   (children :initform nil
-             :initarg :children)
-   (parent :initform nil
-           :initarg :parent))
-  "A node in a tree containing a gumshoe entry.")
-
-(defclass gumshoe--tree ()
-  (
-   (root :initform nil
-         :initarg :root
-         :documentation "Root of the tree.")
-   (current :initform nil
-            :initarg :current
-            :documentation "Current node in the tree."))
-  "A tree to keep track of the target’s movements as they move forward and backward in time.")
-
-(cl-defmethod gumshoe--insert ((self gumshoe--tree) (new gumshoe--node))
-  (if (not self)
-      (gumshoe--tree self :root new :current new)
-    (if (not (oref self root))
-        (progn (oset self root new) (oset self current new))
-      (let* ((current (oref self current)))
-        (oset current children (cons new (oref current children)))
-        (oset new parent current)
-        (oset self current new)
-        )))
-  self
-  )
-
-(cl-defmethod gumshoe--preorder ((self gumshoe--tree))
-  (let* (
-         (stk (list (oref self root)))
-         (preorder nil)
-         (i 0)
-         iter
-         )
-    (while stk
-      (setq iter (pop stk))
-      (push iter preorder)
-      (dolist (child (oref iter children)) (setf stk (cons child stk)))
-      (cl-incf i))
-    preorder)  )
-
-(cl-defmethod gumshoe--dfs ((self gumshoe--tree) (key gumshoe--node))
-  (let* (
-         (stk (list (oref self root)))
-         (i 0)
-         iter
-         (continuep t)
-         foundp)
-    (while (and stk continuep)
-      (setq iter (pop stk))
-      (dolist (child (oref iter children)) (setf stk (cons child stk)))
-      (when (eq iter key)
-        (setq continuep nil)
-        (setf foundp t))
-      (cl-incf i))
-    foundp))
-
-(cl-defmethod gumshoe--delete ((self gumshoe--node))
-  (when self
-    (when (oref self parent)
-      (oset (oref self parent) children (oref self children)))
-    (dolist (child (oref self children))
-      (oset child parent (oref self parent)))))
-
-(cl-defmethod gumshoe--remove ((self gumshoe--tree) (key gumshoe--node))
-  (when (eq key (oref self current))
-    (oset self current (oref (oref self current) parent)))
-  (when (eq key (oref self root))
-    (oset self root (car (oref (oref self root) children))))
-  (gumshoe--delete key))
-
-(cl-defmethod gumshoe--clean-root ((self gumshoe--tree))
+(cl-defmethod gumshoe--clean-root ((self etree--tree))
   (let* (
          (timeline (gumshoe--construct-timeline-nodes self))
          iter
@@ -91,7 +15,7 @@
       (setf timeline (cdr timeline))))
   self)
 
-(cl-defmethod gumshoe--clean-recent ((self gumshoe--tree))
+(cl-defmethod gumshoe--clean-recent ((self etree--tree))
   "Cleanup recent dead entries from RING."
   (unless (eq (oref self current) (oref self root))
     (let ((iter (oref self current))
@@ -102,43 +26,13 @@
             (gumshoe--remove self iter)
           (setq continuep nil))
         (setq iter (oref self current)))
-      ))
-  )
+      )))
 
-(cl-defmethod gumshoe--mapc ((self gumshoe--tree) f)
-  (let* (
-         (stk (list (oref self root)))
-         (preorder nil)
-         (i 0)
-         iter
-         )
-    (while stk
-      (setq iter (pop stk))
-      (funcall f iter)
-      (push iter preorder)
-      (dolist (child (oref iter children)) (setf stk (cons child stk)))
-      (cl-incf i))
-    preorder))
-
-(cl-defmethod gumshoe--collect ((self gumshoe--tree) predicate)
-  (let* (
-         (stk (list (oref self root)))
-         (preorder nil)
-         (i 0)
-         iter
-         )
-    (while (and stk (car stk))
-      (setq iter (pop stk))
-      (when (funcall predicate iter)
-        (push iter preorder))
-      (dolist (child (oref iter children)) (setf stk (cons child stk)))
-      (cl-incf i))
-    preorder))
-(cl-defmethod gumshoe--dead-p ((self gumshoe--node))
+(cl-defmethod gumshoe--dead-p ((self etree--node))
   (if self
       (gumshoe--dead-p (oref self entry))
     t))
-(cl-defmethod gumshoe--clean ((self gumshoe--tree))
+(cl-defmethod gumshoe--clean ((self etree--tree))
   (unless (eq (oref self current) (oref self root))
     (gumshoe--clean-recent self)
     (gumshoe--clean-root self)
@@ -146,26 +40,26 @@
           (gumshoe--collect self 'gumshoe--dead-p)))
   )
 
-(cl-defmethod gumshoe--remove-footprint-entry ((self gumshoe--tree) footprint)
+(cl-defmethod gumshoe--remove-footprint-entry ((self etree--tree) footprint)
   (gumshoe--remove self (overlay-get footprint 'container))
   (delete-overlay footprint))
 
-(cl-defmethod gumshoe--remove-footprint-entries-at ((self gumshoe--tree) position)
+(cl-defmethod gumshoe--remove-footprint-entries-at ((self etree--tree) position)
   (mapc (apply-partially #'gumshoe--remove-footprint-entry self)
         (gumshoe--footprints-at position)))
 
 ;;; filter predicates
-(cl-defmethod gumshoe--in-current-buffer-p ((self gumshoe--node))
+(cl-defmethod gumshoe--in-current-buffer-p ((self etree--node))
   "Check if ENTRY in the current perspective."
   (let ((entry (oref self entry)))
     (equal (oref entry buffer) (current-buffer))))
 
-(cl-defmethod gumshoe--in-current-window-p ((self gumshoe--node))
+(cl-defmethod gumshoe--in-current-window-p ((self etree--node))
   "Check if ENTRY in the current window."
   (let ((entry (oref self entry)))
     (equal (oref entry window) (get-buffer-window (current-buffer)))))
 
-(cl-defmethod gumshoe--construct-timeline-nodes ((self gumshoe--tree))
+(cl-defmethod gumshoe--construct-timeline-nodes ((self etree--tree))
   (let ((stk (list (oref self root)))
         timeline
         (continuep t)
@@ -177,19 +71,19 @@
       (setf iter (oref iter parent))
       (when (eq iter (oref self root)) (setf continuep nil)))
     timeline))
-(cl-defmethod gumshoe--construct-timeline ((self gumshoe--tree))
+(cl-defmethod gumshoe--construct-timeline ((self etree--tree))
   (mapcar (lambda (node) (oref node entry))
           (gumshoe--construct-timeline-nodes self)))
 
-(cl-defmethod gumshoe--add-entry ((self gumshoe--tree) (entry gumshoe--entry))
+(cl-defmethod gumshoe--add-entry ((self etree--tree) (entry gumshoe--entry))
   "Add ENTRY to SELF"
-  (gumshoe--insert self (gumshoe--node :entry entry))
+  (gumshoe--insert self (etree--node :entry entry))
   (when (eq gumshoe-footprint-strategy 'delete-overlapping)
     (gumshoe--remove-footprint-entries-at self (point)))
   (let ((overlay (make-overlay (point) (point))))
     (overlay-put overlay 'container entry)
     (oset entry footprint-overlay overlay)))
-(cl-defmethod gumshoe--log-if-necessary ((self gumshoe--tree) &optional alarmp)
+(cl-defmethod gumshoe--log-if-necessary ((self etree--tree) &optional alarmp)
   "Check current position and log in RING if significant.
 
 Log automatically if ALARMP is t."
@@ -205,6 +99,6 @@ Log automatically if ALARMP is t."
         (gumshoe--add-entry self new-entry)))))
 
 (defun gumshoe--backlog-init (log-len)
-  (gumshoe--tree))
+  (etree--tree))
 
 (provide 'gumshoe-tree)
